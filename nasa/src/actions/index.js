eb947a0d00c905570e6fc3aff397c90c27f62ccc
vanesa-tamu <vahe5495@tamu.edu>
2020-03-18 18:38:06 -0500

    dep installed correctly and connected state to store in redux

--- nasa/src/actions/index.js
+++ nasa/src/actions/index.js
@@ -1,5 +1,8 @@
-export const    APOD_START = 'APOD_START'
-                APOD_SUCCESS = 'APOD_SUCCESS'
-                APOD_FAILURE = 'APOD_FAILURE'
+export const APOD_START = 'APOD_START'
+             APOD_SUCCESS = 'APOD_SUCCESS'
+             APOD_FAILURE = 'APOD_FAILURE'
+
+
+
 
 
